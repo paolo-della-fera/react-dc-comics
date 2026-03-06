3df0b19d25db5c2c8c8e3cc04c711decb2743513
paolo-della-fera <paolodf2005@gmail.com>
2026-03-06 15:59:09 +0100

    add header layout and styles and add dc icon

--- src/components/AppHeader.jsx
+++ src/components/AppHeader.jsx
@@ -1,7 +1,32 @@
+import dcLogo from '/dc-logo.png'
+
 export default function AppHeader() {
 
     return (
-        <header>Header</header>
+        <header>
+            <div className="container">
+                <nav className="navbar d-flex justify-content-between align-items-center">
+
+                    <a className="navbar-brand" href="#">
+                        <img src={dcLogo} alt="DC Logo" />
+                    </a>
+
+                    <div className="d-flex gap-3">
+                        <a className="nav-link text-uppercase" href="#">characters</a>
+                        <a className="nav-link text-uppercase" href="#">comics</a>
+                        <a className="nav-link text-uppercase" href="#">movies</a>
+                        <a className="nav-link text-uppercase" href="#">tv</a>
+                        <a className="nav-link text-uppercase" href="#">games</a>
+                        <a className="nav-link text-uppercase" href="#">collectibles</a>
+                        <a className="nav-link text-uppercase" href="#">videos</a>
+                        <a className="nav-link text-uppercase" href="#">fans</a>
+                        <a className="nav-link text-uppercase" href="#">news</a>
+                        <a className="nav-link text-uppercase" href="#">shop</a>
+                    </div>
+
+                </nav>
+            </div>
+        </header>
     )
 
 }

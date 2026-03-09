73369749a6e44f69dd6874b9aa51eb3319bea356
paolo-della-fera <paolodf2005@gmail.com>
2026-03-09 16:02:36 +0100

    add responsive nav-bar

--- src/components/AppHeader.jsx
+++ src/components/AppHeader.jsx
@@ -58,25 +58,28 @@ export default function AppHeader() {
     return (
         <header>
             <div className="container">
-                <nav className="navbar d-flex justify-content-between align-items-center">
+                <nav className="navbar navbar-expand-lg">
 
                     <a className="navbar-brand" href="#">
                         <img src={dcLogo} alt="DC Logo" />
                     </a>
 
-                    <div className='d-flex gap-4 fw-bold'>
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navMenu">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
 
-                        {
-                            navMenu.map(menu => (
-                                <a className={`nav-link text-uppercase ${menu.active ? 'active-link' : ''}`} key={menu.id} href="#">{menu.text}</a>
-                            ))
-                        }
-
+                    <div className="collapse navbar-collapse" id="navMenu">
+                        <div className='d-flex flex-column flex-lg-row gap-4 fw-bold ms-auto'>
+                            {
+                                navMenu.map(menu => (
+                                    <a className={`nav-link text-uppercase ${menu.active ? 'active-link' : ''}`} key={menu.id} href="#">{menu.text}</a>
+                                ))
+                            }
+                        </div>
                     </div>
-                    
+
                 </nav>
             </div>
         </header>
     )
-
 }

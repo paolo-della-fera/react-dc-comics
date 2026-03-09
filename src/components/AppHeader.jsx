da2316cf3a64172bfb075feff99d87bc73381a06
paolo-della-fera <paolodf2005@gmail.com>
2026-03-09 15:44:14 +0100

    add dynamic nav menu to AppHeader

--- src/components/AppHeader.jsx
+++ src/components/AppHeader.jsx
@@ -1,5 +1,58 @@
 import dcLogo from '/dc-logo.png'
 
+
+const navMenu = [
+    {
+        id: 1,
+        text: 'characters',
+        active: false
+    },
+    {
+        id: 2,
+        text: 'comics',
+        active: true
+    },
+    {
+        id: 3,
+        text: 'movies',
+        active: false
+    },
+    {
+        id: 4,
+        text: 'tv',
+        active: false
+    },
+    {
+        id: 5, text: 'games',
+        active: false
+    },
+    {
+        id: 6,
+        text: 'collectibles',
+        active: false
+    },
+    {
+        id: 7,
+        text: 'videos',
+        active: false
+    },
+    {
+        id: 8,
+        text: 'fans',
+        active: false
+    },
+    {
+        id: 9,
+        text: 'news',
+        active: false
+    },
+    {
+        id: 10,
+        text: 'shop',
+        active: false
+    },
+]
+
 export default function AppHeader() {
 
     return (
@@ -11,19 +64,16 @@ export default function AppHeader() {
                         <img src={dcLogo} alt="DC Logo" />
                     </a>
 
-                    <div className="d-flex gap-4 fw-bold">
-                        <a className="nav-link text-uppercase" href="#">characters</a>
-                        <a className="nav-link text-uppercase active-link" href="#">comics</a>
-                        <a className="nav-link text-uppercase" href="#">movies</a>
-                        <a className="nav-link text-uppercase" href="#">tv</a>
-                        <a className="nav-link text-uppercase" href="#">games</a>
-                        <a className="nav-link text-uppercase" href="#">collectibles</a>
-                        <a className="nav-link text-uppercase" href="#">videos</a>
-                        <a className="nav-link text-uppercase" href="#">fans</a>
-                        <a className="nav-link text-uppercase" href="#">news</a>
-                        <a className="nav-link text-uppercase" href="#">shop</a>
-                    </div>
+                    <div className='d-flex gap-4 fw-bold'>
 
+                        {
+                            navMenu.map(menu => (
+                                <a className={`nav-link text-uppercase ${menu.active ? 'active-link' : ''}`} key={menu.id} href="#">{menu.text}</a>
+                            ))
+                        }
+
+                    </div>
+                    
                 </nav>
             </div>
         </header>
